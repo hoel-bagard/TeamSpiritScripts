f3a9d8e1ddf8f5c6a8b5930768dcae7c7a8e842e
Hoel Bagard <hoel.bagard@gmail.com>
2020-07-31 16:57:56 +0900

    Tried using callbacks, not working

--- remote.js
+++ remote.js
@@ -1,5 +1,5 @@
 // Note:
-// There is a "Date Range" option in TeamSpirit, if you have worked remotely for an extended 
+// There is a "Date Range" option in TeamSpirit, if you have worked remotely for an extended
 // period of time, then consider using it to avoid spamming you manager.
 
 // Also, don't forget to put the "Office" remark before running this script.
@@ -14,52 +14,73 @@ var options_day = {day: '2-digit'};
 var options_month = {month: '2-digit'};
 var date = new Date(year, month, 1);
 var year_month = String(year) + "-" + String(d.toLocaleDateString("en-US", options_month)) + "-";
+var DEBUG = true;
+
+
+// Takes care of filling the pop-up
+function fill_remote(date, callback){
+    // Wait for pop-up to appear and then fill it
+    var change_req = document.getElementById("applyNew_patternS");
+    if (change_req) {
+        console.log("Pop-up loaded");
+        // This skips week-ends and holydays  ((if date.getDay() != 0 && date.getDay() != 6) doesn't work for holidays)
+        if (change_req.childNodes[0].className == "empApplyMenuOn") {
+            change_req.click();
+            document.getElementById("dlgApplyPatternList1").value = "a0V2u0000000VwSEAU";
+            document.getElementById("empApplyDone1").click();
+            if (DEBUG) console.log("Sent request");
+        }
+        else{
+            if (DEBUG) console.log("Was a week-end / holyday");
+            document.getElementById("dialogApplyClose").click();
+            if (DEBUG) console.log("Closed window");
+        }
+        if (typeof callback == "function") callback(date);
+    }
+    else setTimeout(function() { fill_remote(); }, 100);  // check every 100ms
+}
+
+
+function go_to_next_day(date){
+    console.log("Going to the next day");
+    date.setDate(date.getDate() + 1);
+    // Check what is the next day to fill. If it belongs to the current month then we continue, otherwise we stop.
+    if (date.getMonth() === month) autofill(date);
+    else console.log("Finished !");
+}
 
 
 // This function asks for approval to telework for the days of the month one by one (recursively).
 function autofill(date) {
     // If we're on the main page, i.e. if TeamSpirit has finished saving the previous modification
-    if (document.getElementById("startTime") == null) {  // This might be broken
+    if (document.getElementById("startTime") == null && document.getElementById("dateColumn") != null) {
         day = String(date.toLocaleDateString("en-US", options_day));
+        if (DEBUG) console.log("Day: " + date);
 
         // Early stop option
         var stop = false;
         var remark = document.getElementById("dailyNote" + year_month + day);
         if (remark.childNodes[0].childNodes[0].textContent.toUpperCase().trim() == "STOP") {
-            stop = true;
-            console.log("Early stopping")
+            console.log("Early stopping");
         }
 
-        // If the day is a week-end or a holiday, then we just go to the next day. (Can't work on w.e. ==> Can't click them)
-        try {
+        else {
             // Do not apply for telework if there is an "Office" remark for that day
-            var remark = document.getElementById("dailyNote" + year_month + day);
             if (remark.childNodes[0].childNodes[0].textContent.toUpperCase().trim() != "OFFICE") {
                 var request = document.getElementById("ttvApply" + year_month + day);
                 // If there has already been a request for that day, then we skip it
                 if (request.title == "Attendance Related Request") {
                     document.getElementById("ttvApply" + year_month + day).click();
-                    var change_req = document.getElementById("applyNew_patternS");
-                    // This skips week-ends and holydays  ((if date.getDay() != 0 && date.getDay() != 6) doesn't work for holidays)
-                    if (change_req.childNodes[0].className == "empApplyMenuOn") {
-                        change_req.click();
-                        document.getElementById("dlgApplyPatternList1").value = "a0V2u0000000VwSEAU";
-                        document.getElementById("empApplyDone1").click();
-                    }
-                    else{
-                        document.getElementById("dialogApplyClose").click();
-                    }
+                    fill_remote(date, function(date) {
+                        if (DEBUG) console.log("Pop-up filled");
+                        go_to_next_day(date);
+                    });
                 }
+                else { go_to_next_day(date); }
             }
-        }
-        catch (err) {}
-        date.setDate(date.getDate() + 1);
-        // Check what is the next day to fill. If it belongs to the current month then we continue, otherwise we stop.
-        if (date.getMonth() === month && !stop){
-            autofill(date);
-        }
-        else{
-            console.log("Finished !");
+            else {
+                go_to_next_day(date);
+            }
         }
     }
     // If TeamSpirit is still saving the modification (the "Please wait" pop-up), then we just wait 0.1s and try again
